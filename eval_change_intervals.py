48eafd33f5474174004892141064dbca45a6c4f5
habakan <kansukebano@gmail.com>
2023-06-11 18:17:52 +0900

    add save csv in eval_change_intervals.py

--- eval_change_intervals.py
+++ eval_change_intervals.py
@@ -1,5 +1,7 @@
 import numpy as np
 import argparse
+import csv
+import pathlib
 
 
 def read_file(path):
@@ -33,10 +35,13 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--dataset", default="gtea")
-    parser.add_argument("--neighbor_frames", type=int, default=10)
     parser.add_argument("--split", default="1")
+    parser.add_argument('--feature_name', default='features')
+    parser.add_argument('--config', default='./configs/example.yaml')
 
     args = parser.parse_args()
+
+    output_path = pathlib.Path('/work/results/'+'ms-tcn_' + args.dataset+'_change_intervals.csv')
 
     ground_truth_path = "./data/" + args.dataset + "/groundTruth/"
     recog_path = "./results/" + args.dataset + "/split_" + args.split + "/"
@@ -47,22 +52,32 @@ def main():
     correct = 0
     total = 0
 
-    for vid in list_of_videos:
-        gt_file = ground_truth_path + vid
-        gt_content = read_file(gt_file).split("\n")[0:-1]
+    acc_list = []
+    for neighbor_frames in [10, 20, 30]:
+        for vid in list_of_videos:
+            gt_file = ground_truth_path + vid
+            gt_content = read_file(gt_file).split("\n")[0:-1]
 
-        recog_file = recog_path + vid.split(".")[0]
-        recog_content = read_file(recog_file).split("\n")[1].split()
+            recog_file = recog_path + vid.split(".")[0]
+            recog_content = read_file(recog_file).split("\n")[1].split()
 
-        change_indices = get_change_indices(gt_content)
-        eval_indices = get_eval_indices(change_indices, args.neighbor_frames)
+            change_indices = get_change_indices(gt_content)
+            eval_indices = get_eval_indices(change_indices, neighbor_frames)
 
-        for i in eval_indices:
-            total += 1
-            if gt_content[i] == recog_content[i]:
-                correct += 1
+            for i in eval_indices:
+                total += 1
+                if gt_content[i] == recog_content[i]:
+                    correct += 1
+        acc = (100 * float(correct) / total)
+        print("Acc: %.4f" % acc)
+        acc_list.append(acc)
 
-    print("Acc: %.4f" % (100 * float(correct) / total))
+    with output_path.open('a') as csvfile:
+        evalwriter = csv.writer(csvfile, delimiter=',')
+        if not output_path.exists():
+            evalwriter.writerow(['config_path', 'feature_name', 'fold', 'Acc10', 'Acc20', 'Acc30'])
+        evalwriter.writerow([args.config, args.feature_name, args.split] + acc_list)
+
 
 
 if __name__ == "__main__":
